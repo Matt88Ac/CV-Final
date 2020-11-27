0d848a63e712e0062cb6bceb167d5689d32eee8b
Matt8ac <Matt8ac@gmail.com>
2020-11-27 19:45:56 +0200

    Update preProcessing.py

--- preProcessing.py
+++ preProcessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 
 class preProcessing:
@@ -7,19 +8,95 @@ class preProcessing:
     def __init__(self, image: np.ndarray):
         self.image: np.ndarray = image.copy()
 
-        def runPP(matrix: np.ndarray) -> np.ndarray:
-            img = cv2.cvtColor(matrix.copy(), cv2.COLOR_BGR2GRAY)
+        self.binIm = self.__runPP()
+        self.original_area = self.__findArea()
+        self.gray_area, self.gray_M = self.__four_point_transform(self.binIm, self.original_area)
+        self.original_area, self.original_M = self.__four_point_transform(self.image, self.original_area)
 
-            img = cv2.GaussianBlur(img, (9, 9), 0)
-            img = cv2.fastNlMeansDenoising(img, None)
-            img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        # self.gray_area: np.ndarray = self.gray_area[10:, :]
+        # self.original_area: np.ndarray = self.original_area[10:, :]
 
-            img = cv2.bitwise_not(img, img)
+    def __runPP(self) -> np.ndarray:
+        imag = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-            kernel = np.ones((2, 2), np.uint8)
+        imag = cv2.GaussianBlur(imag, (9, 9), 0)
+        imag = cv2.fastNlMeansDenoising(imag, None)
+        imag = cv2.adaptiveThreshold(imag, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-            img = cv2.dilate(img, kernel, iterations=1)
+        imag = cv2.bitwise_not(imag, imag)
 
-            return img
+        kernel = np.ones((2, 2), np.uint8)
 
-        self.grayIm = runPP(image)
+        imag = cv2.dilate(imag, kernel, iterations=1)
+
+        return imag
+
+    def __findArea(self) -> np.ndarray:
+        contours, _ = cv2.findContours(self.binIm.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        largestContours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
+
+        epsilon = 0.015 * cv2.arcLength(largestContours[0], True)
+        approx = cv2.approxPolyDP(largestContours[0], epsilon, True)
+
+        return approx
+
+    @staticmethod
+    def __four_point_transform(image: np.ndarray, pts: np.ndarray):
+        imag = image.copy()
+
+        def order_points(points: np.ndarray):
+            rect1 = np.zeros((4, 2), dtype=np.float32)
+
+            s = points.sum(axis=1)
+            rect1[0] = points[np.argmin(s)]
+            rect1[2] = points[np.argmax(s)]
+
+            diff = np.diff(points, axis=1)
+            rect1[1] = points[np.argmin(diff)]
+            rect1[3] = points[np.argmax(diff)]
+
+            # return the ordered coordinates
+            return rect1
+
+        t_pts = pts.reshape(4, 2).copy()
+        # obtain a consistent order of the points and unpack them
+        # individually
+        rect = order_points(t_pts)
+        (tl, tr, br, bl) = rect
+
+        # compute the width of the new image, which will be the
+        # maximum distance between bottom-right and bottom-left
+        # x-coordinates or the top-right and top-left x-coordinates
+
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+
+        # compute the height of the new image, which will be the
+        # maximum distance between the top-right and bottom-right
+        # y-coordinates or the top-left and bottom-left y-coordinates
+
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+
+        # now that we have the dimensions of the new image, construct
+        # the set of destination points to obtain a "birds eye view",
+        # (i.e. top-down view) of the image, again specifying points
+        # in the top-left, top-right, bottom-right, and bottom-left
+        # order
+
+        dst = np.array([[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]], dtype=np.float32)
+
+        # compute the perspective transform matrix and then apply it
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(imag, M, (maxWidth, maxHeight), borderMode=cv2.BORDER_TRANSPARENT)
+        return warped, M
+
+
+
+
+# img = cv2.imread('sudoku.jpg')
+# pp = preProcessing(img)
+# plt.imshow(pp.original_area, cmap='gray')
+# plt.show()
