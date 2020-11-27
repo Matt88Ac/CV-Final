60d36c737ce96e8e73d33ece23af3eaca3277001
Matt8ac <Matt8ac@gmail.com>
2020-11-27 20:42:25 +0200

    Update preProcessing.py

--- preProcessing.py
+++ preProcessing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 
 class preProcessing:
@@ -109,20 +111,59 @@ class preProcessing:
         plt.show()
 
 
-class SVM:
+class DigitsSVM:
 
-    def __init__(self):
+    def __init__(self, split: float = 0.7):
         self.svm = cv2.ml.SVM_create()
         self.svm.setType(cv2.ml.SVM_C_SVC)
         self.svm.setKernel(cv2.ml.SVM_LINEAR)
 
         self.digits_dataset = cv2.imread('digitst.jpg', 0)
-        self.digits_dataset = np.array([np.hsplit(row, 9) for
-                                        row in np.vsplit(self.digits_dataset, 40)]).reshape(-1, 2500)
+        self.digits_dataset: np.ndarray = np.array([np.hsplit(row, 9) for
+                                                    row in np.vsplit(self.digits_dataset, 40)]).reshape(-1, 2500)
 
+        self.digits_dataset: np.ndarray = self.digits_dataset.reshape((360, 50, 50))
 
+        winSize = (40, 40)
+        blockSize = (20, 20)
+        blockStride = (10, 10)
+        cellSize = (5, 5)
+        nbins = 9
+        self.hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
 
+        self.labels = np.tile(np.arange(1, 10), int(len(self.digits_dataset) / 9))
+        xtest, ytest = self.__split_and_train(split)
 
-img = cv2.imread('sudoku.jpg')
-pp = preProcessing(img)
-pp.plot()
+        nbins = self.svm.predict(xtest)
+        self.score = accuracy_score(nbins[1], ytest)
+        self.con_mat = confusion_matrix(nbins[1], ytest)
+
+    def predict(self, digit_image: np.ndarray) -> int:
+        im = cv2.resize(digit_image, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
+        im = im.reshape(50, 50).astype(np.uint8)
+        im = self.hog.compute(im)
+        return self.svm.predict(np.array([im]))[1][0][0]
+
+    def __split_and_train(self, split) -> tuple:
+        n = len(self.digits_dataset)
+
+        x_train = self.digits_dataset[: int(n * split)]
+        y_train = self.labels[: int(n * split)]
+
+        x_test = self.digits_dataset[int(n * split):]
+        y_test = self.labels[int(n * split):]
+
+        x_train = np.array([self.hog.compute(x0) for x0 in x_train], dtype=np.float32)
+        x_test = np.array([self.hog.compute(x0) for x0 in x_test], dtype=np.float32)
+
+        self.svm.train(x_train, cv2.ml.ROW_SAMPLE, y_train.astype(np.int32))
+
+        return x_test, y_test
+
+# svm = DigitsSVM()
+# d1 = svm.digits_dataset[5]
+# pred = svm.predict(d1)
+# print(svm.score)
+# img = cv2.imread('sudoku.jpg')
+# pp = preProcessing(img)
+# pp.plot()
