31d2562260d856010ea6d8c03004ae1709253379
Matt8ac <Matt8ac@gmail.com>
2020-11-28 22:21:35 +0200

    Update Sudoku.py
    problems solved I hope

--- Sudoku.py
+++ Sudoku.py
@@ -10,56 +10,69 @@ class Cells:
         self.prep = preProcessor(sudoku)
         color = (50, 120, 200)
 
-        def plotLines(imag: np.ndarray, edges: np.ndarray):
-            for x1, y1, x2, y2 in edges[:, 0, :]:
-                cv2.line(imag, (x1, y1), (x2, y2), color, 2)
-            return imag
-
-        lines = cv2.HoughLinesP(self.prep.gray_area, 1, np.pi / 40, 180, maxLineGap=250, minLineLength=60)
-        close = plotLines(cv2.cvtColor(self.prep.gray_area, cv2.COLOR_GRAY2BGR), lines)
-
-        gray = cv2.cvtColor(close, cv2.COLOR_BGR2GRAY)
-        blur = cv2.medianBlur(gray, 3)
-        sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-        sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
-
-        thresh = cv2.threshold(sharpen, 100, 250, cv2.THRESH_BINARY_INV)[1]
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        close = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-        close = cv2.morphologyEx(close, cv2.MORPH_CLOSE, kernel, iterations=1)
-
-        cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        self.raw = np.array([np.array_split(row, 9, axis=1)
+                             for row in np.array_split(self.prep.gray_area, 9)]).reshape(81, 1)
 
         self.cells = np.zeros((81, 50, 50))
+        self.original = np.array([np.array_split(row, 9, axis=1)
+                                  for row in np.array_split(self.prep.original_area, 9)]).reshape(81, 1)
 
-        ww = np.array([cv2.boundingRect(c)[2] for c in cnts])
-        xx = []
-        yy = []
+        def CompleteClassifier(img: np.ndarray) -> np.ndarray:
 
-        i = 0
-        for c in cnts:
-            x, y, w, h = cv2.boundingRect(c)
-            if np.quantile(ww, 0.099) <= w <= np.quantile(ww, 0.911):
-                temp = self.prep.gray_area[y:y + h, x:x + w]
-                temp = cv2.resize(temp, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
-                self.cells[i] = temp.copy()
-                xx.append(y)
-                yy.append(x)
-                i += 1
+            def plotLines(imag: np.ndarray, edges: np.ndarray):
+                for x1, y1, x2, y2 in edges[:, 0, :]:
+                    cv2.line(imag, (x1, y1), (x2, y2), color, 2)
+                return imag
 
-        # taking care of positions, as they are in the original image
-        self.cells = self.cells[np.argsort(xx)]
-        self.cells = self.cells.reshape((9, 9, 50, 50))
-        yy = np.array(yy)
-        yy = yy[np.argsort(xx)].reshape(9, 9)
+            lines = cv2.HoughLinesP(img, 1, np.pi / 40, 180, maxLineGap=250, minLineLength=60)
+            close = plotLines(cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), lines)
 
-        for i in range(9):
-            self.cells[i] = self.cells[i][np.argsort(yy[i])]
+            gray = cv2.cvtColor(close, cv2.COLOR_BGR2GRAY)
+            blur = cv2.medianBlur(gray, 3)
+            sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+            sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
 
-        self.cells = self.cells.reshape(81, 50, 50)
+            thresh = cv2.threshold(sharpen, 100, 250, cv2.THRESH_BINARY_INV)[1]
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+            close = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+            close = cv2.morphologyEx(close, cv2.MORPH_CLOSE, kernel, iterations=1)
 
-    def __getitem__(self, item):
+            cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+            ww = np.array([cv2.boundingRect(c)[2] for c in cnts])
+            xx = []
+            yy = []
+
+            tempo = self.cells.copy()
+
+            i = 0
+            for c in cnts:
+                x, y, w, h = cv2.boundingRect(c)
+                if np.quantile(ww, 0.099) <= w <= np.quantile(ww, 0.911):
+                    temp = self.prep.gray_area[y:y + h, x:x + w]
+                    temp = cv2.resize(temp, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
+                    tempo[i] = temp.copy()
+                    xx.append(y)
+                    yy.append(x)
+                    i += 1
+
+            # taking care of positions, as they are in the original image
+            tempo = tempo[np.argsort(xx)]
+            tempo = tempo.reshape((9, 9, 50, 50))
+            yy = np.array(yy)
+            yy = yy[np.argsort(xx)].reshape(9, 9)
+
+            for i in range(9):
+                tempo[i] = tempo[i][np.argsort(yy[i])]
+
+            tempo = tempo.reshape(81, 50, 50)
+
+            return tempo
+
+        self.cells = CompleteClassifier(self.prep.gray_area)
+
+    def __getitem__(self, item) -> np.ndarray:
         return self.cells[item]
 
     def __improve(self):
@@ -74,10 +87,11 @@ class Cells:
 
     def plot(self):
         k = 1
-        for i in range(81):
+        for i in range(self.cells.shape[0]):
             plt.subplot(9, 9, k)
             k += 1
-            plt.imshow(self.cells[i], cmap='gray')
+            # plt.imshow(self.cells[i], cmap='gray')
+            plt.imshow(self.raw[i][0], cmap='gray')
             plt.xticks([]), plt.yticks([])
 
         plt.show()
@@ -87,7 +101,8 @@ class Digits:
 
     def __init__(self, sudoku: np.ndarray):
         self.cells = Cells(sudoku)
-        self.digits = np.array([self.__extract_digit(i) for i in range(len(self.cells))])
+        self.digits = np.array([self.__extract_digit(i) for i in range(len(self.cells.raw))])
+
         self.images = self.digits[:, 1]
         self.digits = self.digits[:, 0]
 
@@ -96,16 +111,14 @@ class Digits:
         pred_cells = [self.svm.predict(c) for c in self.cells]
 
         for i in range(len(pred_cells)):
-            if pred_cells[i] != 1 and self.digits[i] is None:
+            if self.images[i].sum() < self.cells[i].sum() and pred_cells[i] != 1 and self.digits[i] is None:
                 self.digits[i] = self.cells[i].copy()
                 self.images[i] = self.cells[i].copy()
 
-        self.matrix = np.array([self.svm.predict(d) for d in self.digits])
-
-        print(self.matrix.reshape(9, 9))
+        self.matrix = np.array([self.svm.predict(d) for d in self.digits]).reshape(9, 9)
 
     def __extract_digit(self, which, kernel_size: tuple = (5, 5)):
-        im = self.cells[which].copy().astype(np.uint8)
+        im = self.cells.raw[which][0]  # self.cells[which].copy().astype(np.uint8)
         r_w = 50
         r_h = 50
 
@@ -164,6 +177,81 @@ class Digits:
         plt.show()
 
 
+class Sudoku:
+
+    def __init__(self, grid: np.ndarray):
+        self.digits = Digits(grid)
+
+        self.solution = self.__solve()
+
+    def __solve(self):
+
+        def is_safe(array, r, c, num) -> bool:
+            def already_used(n, a):
+                return n in a
+
+            box_r = r - r % 3
+            box_c = c - c % 3
+            box = already_used(num, array[box_r:box_r + 3, box_c:box_c + 3])
+
+            return not already_used(num, array[r]) and not already_used(num, array[:9, c:c + 1]) and not box
+
+        def solve(arr: np.ndarray):
+            def next_cell(a: np.ndarray) -> tuple:
+                if arr[0][0] == 0:
+                    return 0, 0
+                (r, c) = np.unravel_index((a == 0).argmax(), a.shape)
+                if r != 0 or c != 0:
+                    return r, c
+
+                return -1, -1
+
+            row, col = next_cell(arr)
+
+            # solved
+            if row == col == -1:
+                return True
+
+            for num in range(1, 10):
+                if is_safe(arr, row, col, num):
+                    arr[row][col] = num
+
+                    if solve(arr):
+                        return True
+
+                    arr[row][col] = 0
+
+            # no solution
+            return False
+
+        def should_try(sudoku: np.ndarray) -> bool:
+            for i in range(3):
+                for j in range(3):
+                    box = sudoku[i * 3:i * 3 + 3, j * 3:j * 3 + 3]
+                    if len(box[box != 0]) < 2:
+                        return False
+
+            # if current sudoku is valid
+            # check if each cell value (from SVM) is valid (no duplicate, etc..)
+            for i in range(9):
+                for j in range(9):
+                    arr = sudoku.copy()
+                    num = arr[i, j]
+                    if num != 0:
+                        arr[i, j] = 0
+                        if not is_safe(arr, i, j, num):
+                            return False
+            return True
+
+        solved = self.digits.matrix.copy()
+
+        if should_try(solved):
+            if solve(solved):
+                return solved
+
+        return None
+
+
 image = cv2.imread('sudoku.jpg')
-digs = Digits(image)
-digs.plot()
+sud = Sudoku(image)
+print(sud.solution)
