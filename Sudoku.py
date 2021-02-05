ae4ba039b50024c67616a951cbf702566e360a29
Matt8ac <Matt8ac@gmail.com>
2021-02-05 20:59:31 +0200

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -2,41 +2,73 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib.image import AxesImage
-from preProcessing import DigitsSVM, preProcessor
+from preProcessing import preProcessor
+from ML_Models import DigitsSVM
 import os
 from datetime import datetime
+import imutils
 
 
 class Cells:
 
     def __init__(self, sudoku: np.ndarray):
         self.prep = preProcessor(sudoku)
-
         color = (50, 120, 200)
+        self.raw = np.array_split(self.prep.gray_area, 9)
+        self.raw = [np.array_split(row, 9, axis=1) for row in self.raw]
+        shapes = np.array([l.shape[1] for r in self.raw for l in r])
+        mn = np.min(shapes)
+        for i in range(len(self.raw)):
+            for j in range(len(self.raw[i])):
+                self.raw[i][j] = self.raw[i][j][:, :mn]
 
-        self.raw = np.array([np.array_split(row, 9, axis=1)
-                             for row in np.array_split(self.prep.gray_area, 9)]).reshape(81, 1)
+        self.raw = np.array(self.raw)
+        self.raw = self.raw.reshape((81,) + self.raw.shape[2:])
 
         self.cells = np.zeros((81, 50, 50))
-        self.original = np.array([np.array_split(row, 9, axis=1)
-                                  for row in np.array_split(self.prep.original_area, 9)]).reshape(81, 1)
+        self.original = np.array_split(self.prep.original_area, 9)
+        self.original = [np.array_split(row, 9, axis=1) for row in self.original]
+        shapes = np.array([l.shape[1] for r in self.original for l in r])
+        mn = np.min(shapes)
+        for i in range(len(self.original)):
+            for j in range(len(self.original[i])):
+                self.original[i][j] = self.original[i][j][:, :mn]
+        self.original = np.array(self.original)
+        self.original = self.original.reshape((81, 1) + self.original.shape[2:])
 
         def CompleteClassifier(self, img: np.ndarray) -> np.ndarray:
 
             def plotLines(imag: np.ndarray, edges: np.ndarray):
                 for x1, y1, x2, y2 in edges[:, 0, :]:
-                    cv2.line(imag, (x1, y1), (x2, y2), color, 2)
+                    cv2.line(imag, (x1, y1), (x2, y2), color, 1)
                 return imag
 
-            lines = cv2.HoughLinesP(img, 1, np.pi / 40, 180, maxLineGap=250, minLineLength=60)
-            close = plotLines(cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), lines)
+            def plotLines2(imag: np.ndarray, edges: np.ndarray):
+                height, width = np.shape(img)
+                for x1, y1 in edges[:, 0, :]:
+                    if y1 != 0:
+                        m = -1 / np.tan(y1)
+                        c = x1 / np.sin(y1)
+                        cv2.line(imag, (0, int(c)), (width, int(m * width + c)), color, thickness=2)
+                    else:
+                        cv2.line(imag, (x1, 0), (x1, height), color, thickness=2)
+                return imag
+
+            # lines = cv2.HoughLinesP(img, 1, np.pi / 40, 180, maxLineGap=280, minLineLength=60)
+            lines = cv2.HoughLines(img, 1, np.pi / 180, 360)
+            close = plotLines2(cv2.cvtColor(img, cv2.COLOR_GRAY2BGR), lines)
             self.lines1 = close.copy()
+            # plt.imshow(close)
+            # plt.show()
 
             gray = cv2.cvtColor(close, cv2.COLOR_BGR2GRAY)
             blur = cv2.medianBlur(gray, 3)
+            # blur = cv2.GaussianBlur(gray, (3, 3), 0)
             sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
             sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
             self.lines2 = sharpen.copy()
+            # plt.imshow(sharpen, cmap='gray')
+            # plt.show()
 
             thresh = cv2.threshold(sharpen, 100, 250, cv2.THRESH_BINARY_INV)[1]
             kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
@@ -45,19 +77,41 @@ class Cells:
             self.lines3 = close.copy()
 
             cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+            # plt.imshow(close, cmap='gray')
+            # plt.show()
 
-            ww = np.array([cv2.boundingRect(c)[2] for c in cnts])
+            areas = np.array([cv2.contourArea(c) for c in cnts])
+            lenW = np.array([cv2.boundingRect(c)[2] for c in cnts])
             xx = []
             yy = []
 
-            tempo = self.cells.copy()
+            tempo = np.zeros((81, 50, 50))
+            Wmn = lenW.mean()
+            Wstd = lenW.std()
 
             i = 0
+            new_img = self.prep.original_area.copy()
             for c in cnts:
                 x, y, w, h = cv2.boundingRect(c)
-                if np.quantile(ww, 0.099) <= w <= np.quantile(ww, 0.911):
+                if h == 0 or w == 0:
+                    continue
+                if 1 / 1.5 > w / h or w / h > 1.5:  # Optionally 1
+                    continue
+                if w < 3 or h < 3:
+                    continue
+
+                cond = np.quantile(areas, 0.1) <= cv2.contourArea(c) <= np.quantile(areas, 0.95)
+                if Wmn - Wstd <= w <= Wmn + Wstd:
+                    # print(w, h)
+                    new_img = cv2.rectangle(new_img, (x, y), (w + x, h + y), color=(240, 70, 130), thickness=3)
                     temp = self.prep.gray_area[y:y + h, x:x + w]
+                    # plt.subplot(1, 2, 1)
+                    # plt.imshow(temp, cmap='gray')
+                    # plt.subplot(1, 2, 2)
+                    # plt.imshow(new_img)
+                    # plt.show()
                     temp = cv2.resize(temp, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
                     tempo[i] = temp.copy()
                     xx.append(y)
@@ -81,7 +135,7 @@ class Cells:
             self.cells = CompleteClassifier(self, self.prep.gray_area)
 
         except ValueError:
-            self.cells = np.zeros((81, 50, 50))
+            self.cells = self.raw.copy()
 
     def __getitem__(self, item) -> np.ndarray:
         return self.cells[item]
@@ -116,7 +170,7 @@ class Cells:
                 plt.subplot(9, 9, k)
                 k += 1
                 plt.xticks([]), plt.yticks([])
-                plt.imshow(self.raw[i][0], cmap='gray')
+                plt.imshow(self.raw[i], cmap='gray')
 
             if save is not None:
                 plt.savefig(save)
@@ -129,25 +183,24 @@ class Digits:
 
     def __init__(self, sudoku: np.ndarray):
         self.cells = Cells(sudoku)
-        self.digits = np.array([self.__extract_digit(i) for i in range(len(self.cells.raw))])
+        # self.cells.plot()
+        # self.cells.prep.plot()
+
+        self.svm = DigitsSVM()
+
+        self.digits = np.array([self.__preprocess_image(i) for i in range(len(self.cells.cells))])
 
         self.images = self.digits[:, 1]
         self.digits = self.digits[:, 0]
-
-        self.svm = DigitsSVM()
-        pred_cells = [self.svm.predict(c) for c in self.cells]
-
-        for i in range(len(pred_cells)):
-            if self.images[i].sum() < self.cells[i].sum() and pred_cells[i] != 1 and self.digits[i] is None:
-                self.digits[i] = self.cells[i].copy()
-                self.images[i] = self.cells[i].copy()
+        pred_cells = np.array([self.svm.predict(c) for c in self.cells.cells]).reshape((9, 9))
 
         self.matrix: np.ndarray = np.array([self.svm.predict(d) for d in self.digits]).reshape(9, 9)
 
     def __extract_digit(self, which, kernel_size: tuple = (5, 5)):
-        im = self.cells.raw[which][0]  # self.cells[which].copy().astype(np.uint8)
-        r_w = 50
-        r_h = 50
+        # im = self.cells.cells[which][0]
+        im = self.cells[which].copy().astype(np.uint8)
+        # plt.imshow(im, cmap='gray')
+        # plt.show()
 
         original = im.copy()
         o_h, o_w = im.shape
@@ -167,30 +220,116 @@ class Digits:
 
         im = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel)
         im = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
+        # plt.imshow(im, cmap='gray')
+        # plt.show()
 
         contours, _ = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours = sorted(contours, key=cv2.contourArea)[::-1]
 
         if len(contours) != 0:
+
+            # c = max(contours, key=cv2.contourArea)
+            # x, y, w, h = cv2.boundingRect(c)
+#
+            # original_area = original[y:y + h, x:x + w]
+            # n_h, n_w = original_area.shape
+#
+            # # validate contours is number and not noise
+            # if n_h > (o_h / 5) and n_w > (o_w / 5):
+            #     res = np.zeros((r_h, r_h))
+            #     dh = r_h - h
+            #     dw = r_w - w
+#
+            #     res[int(dh / 2):-int(dh / 2) - dh % 2, int(dw / 2):-int(dw / 2) - dw % 2] = original_area
+#
+            #     cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), 2)
             # find the biggest area
-            c = max(contours, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(c)
+            # c = max(contours, key=cv2.contourArea)
+            for c in contours:
+                x, y, w, h = cv2.boundingRect(c)
 
-            original_area = original[y:y + h, x:x + w]
-            n_h, n_w = original_area.shape
+                original_area = original[y - 5:min(y + h + 5, o_h), x - 5:min(x + w + 5, o_w)]
+                if np.sum(original_area.astype('float') / 255) < 10:
+                    continue
+                #
+                # if 1 / 1.4 > w / h or w / h > 1.4:  # Optionally 1
+                #     pass
+                # plt.imshow(original_area, cmap='gray')
+                # plt.plot()
 
-            # validate contours is number and not noise
-            if n_h > (o_h / 5) and n_w > (o_w / 5):
-                res = np.zeros((r_h, r_h))
-                dh = r_h - h
-                dw = r_w - w
+                res = cv2.rectangle(cv2.cvtColor(im, cv2.COLOR_GRAY2BGR), (x, y), (x + w, y + h), (200, 150, 70), 2)
 
-                res[int(dh / 2):-int(dh / 2) - dh % 2, int(dw / 2):-int(dw / 2) - dw % 2] = original_area
+                # plt.subplot(1, 3, 1)
+                # plt.imshow(original_area, cmap='gray')
+                # plt.subplot(1, 3, 2)
+                # plt.imshow(im, cmap='gray')
+                # plt.subplot(1, 3, 3)
+                # plt.imshow(res)
+                # plt.show()
+                #
+                # original_area = cv2.GaussianBlur(original_area, (3, 3), 0)
+                # original_area = cv2.fastNlMeansDenoising(original_area, None)
+                # original_area = cv2.morphologyEx(original_area, cv2.MORPH_OPEN, kernel)
+                # original_area = cv2.morphologyEx(original_area, cv2.MORPH_CLOSE, kernel)
 
-                cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), 2)
-
-                return res, im
+                return original_area, res
 
         return None, im
+
+    def __preprocess_image(self, which):
+        img = self.cells[which].astype(np.uint8)
+        rows = np.shape(img)[0]
+
+        for i in range(rows):
+            cv2.floodFill(img, None, (0, i), 0)
+            cv2.floodFill(img, None, (i, 0), 0)
+            cv2.floodFill(img, None, (rows - 1, i), 0)
+            cv2.floodFill(img, None, (i, rows - 1), 0)
+            cv2.floodFill(img, None, (1, i), 1)
+            cv2.floodFill(img, None, (i, 1), 1)
+            cv2.floodFill(img, None, (rows - 2, i), 1)
+            cv2.floodFill(img, None, (i, rows - 2), 1)
+
+        rowtop = None
+        rowbottom = None
+        colleft = None
+        colright = None
+        thresholdBottom = 50
+        thresholdTop = 50
+        thresholdLeft = 50
+        thresholdRight = 50
+        center = rows // 2
+        for i in range(center, rows):
+            if rowbottom is None:
+                temp = img[i]
+                if sum(temp) < thresholdBottom or i == rows - 1:
+                    rowbottom = i
+            if rowtop is None:
+                temp = img[rows - i - 1]
+                if sum(temp) < thresholdTop or i == rows - 1:
+                    rowtop = rows - i - 1
+            if colright is None:
+                temp = img[:, i]
+                if sum(temp) < thresholdRight or i == rows - 1:
+                    colright = i
+            if colleft is None:
+                temp = img[:, rows - i - 1]
+                if sum(temp) < thresholdLeft or i == rows - 1:
+                    colleft = rows - i - 1
+
+        # Centering the bounding box's contents
+        newimg = np.zeros(np.shape(img))
+        startatX = (rows + colleft - colright) // 2
+        startatY = (rows - rowbottom + rowtop) // 2
+        for y in range(startatY, (rows + rowbottom - rowtop) // 2):
+            for x in range(startatX, (rows - colleft + colright) // 2):
+                newimg[y, x] = img[rowtop + y - startatY, colleft + x - startatX]
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        newimg = cv2.morphologyEx(newimg, cv2.MORPH_OPEN, kernel)
+        newimg = cv2.morphologyEx(newimg, cv2.MORPH_CLOSE, kernel)
+
+        return newimg, newimg
 
     def plot(self, save: str = None):
         for i in range(len(self.cells)):
@@ -385,5 +524,5 @@ def createVideo(sudoku: np.ndarray):
     out.release()
 
 
-image = cv2.imread('data/photo_2021-01-18_22-14-37.jpg')
+image = cv2.imread('data/sudoku.jpg')
 createVideo(image)
