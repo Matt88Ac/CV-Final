8ac99e4e0a674ffb7411e335668d2c73b7492dd7
Matt8ac <Matt8ac@gmail.com>
2021-02-08 12:19:16 +0200

    Update ML_Models.py

--- ML_Models.py
+++ ML_Models.py
@@ -11,49 +11,79 @@ from matplotlib import pyplot as plt
 class DigitsSVM:
 
     def __init__(self, split: float = 0.7):
-        self.svm = cv2.ml.SVM_create()
-        self.svm.setType(cv2.ml.SVM_C_SVC)
-        self.svm.setKernel(cv2.ml.SVM_LINEAR)
-
-        self.digits_dataset = cv2.imread('Dataset/set.jpg', 0)
-        self.digits_dataset: np.ndarray = np.array([np.hsplit(row, 9) for
-                                                    row in np.vsplit(self.digits_dataset, 40)]).reshape(-1, 2500)
-
-        self.digits_dataset: np.ndarray = self.digits_dataset.reshape((360, 50, 50))
-
+        s = 50
+        self.s = (s, s)
         winSize = (40, 40)
         blockSize = (20, 20)
         blockStride = (10, 10)
         cellSize = (5, 5)
         nbins = 9
         self.hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
+        if 'SVModel.dat' in os.listdir():
+            self.svm = cv2.ml.SVM_load('SVModel.dat')
+            print('SVM Model Loaded!')
+            return
 
-        self.labels = np.tile(np.arange(1, 10), int(len(self.digits_dataset) / 9))
-        xtest, ytest = self.__split_and_train(split)
+        self.svm: cv2.ml_SVM = cv2.ml.SVM_create()
+        self.svm.setType(cv2.ml.SVM_C_SVC)
+        self.svm.setKernel(cv2.ml.SVM_LINEAR)
+        self.svm.setTermCriteria((cv2.TERM_CRITERIA_COUNT, 100, 1e-6))
+
+        print('Training Model...')
+
+        def openDataSet():
+            digits_dataset_n = []
+            labels = []
+
+            for element in ['Train', 'Test']:
+                for i in range(10):
+                    PATH = 'Dataset/' + element + f'/{i}'
+                    images = os.listdir(PATH)
+                    for image in images:
+                        n_Path = PATH + '/' + image
+                        image = cv2.imread(n_Path, 0)
+                        image = cv2.resize(image, self.s, interpolation=cv2.INTER_CUBIC)
+                        digits_dataset_n.append(image)
+                        labels.append(i)
+            labels = np.array(labels)
+            digits_dataset_n = np.array(digits_dataset_n)
+
+            return digits_dataset_n, labels
+
+        dds, lbls = openDataSet()
+        xtest, ytest = self.__split_and_train(dds, lbls, split)
+        print('Done Training!')
 
         nbins = self.svm.predict(xtest)
         self.score = accuracy_score(nbins[1], ytest)
         self.con_mat = confusion_matrix(nbins[1], ytest)
 
+        self.svm.save('SVModel.dat')
+
     def predict(self, digit_image: np.ndarray) -> int:
+        # plt.imshow(digit_image, cmap='gray')
+        # plt.show()
         if type(digit_image) != np.ndarray:
             return 0
 
-        im = cv2.resize(digit_image, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
-        im = im.reshape(50, 50).astype(np.uint8)
-        if im.sum() < 1000:
+        # im = digit_image.copy().astype(np.uint8)
+        im = cv2.resize(digit_image, self.s, interpolation=cv2.INTER_CUBIC).astype(np.uint8)
+        test = im.astype('float') / 255
+        if test.sum() < 100:
             return 0
-        im = self.hog.compute(im)
-        return self.svm.predict(np.array([im]))[1][0][0]
+        im = np.array(self.hog.compute(im))
+        im = im.reshape((1, len(im)))
+        pred = self.svm.predict(samples=im)
+        return pred[1][0][0]
 
-    def __split_and_train(self, split) -> tuple:
-        n = len(self.digits_dataset)
+    def __split_and_train(self, dds, labels, split) -> tuple:
+        n = len(dds)
 
-        x_train = self.digits_dataset[: int(n * split)]
-        y_train = self.labels[: int(n * split)]
+        x_train = dds[: int(n * split)]
+        y_train = labels[: int(n * split)]
 
-        x_test = self.digits_dataset[int(n * split):]
-        y_test = self.labels[int(n * split):]
+        x_test = dds[int(n * split):]
+        y_test = labels[int(n * split):]
 
         x_train = np.array([self.hog.compute(x0) for x0 in x_train], dtype=np.float32)
         x_test = np.array([self.hog.compute(x0) for x0 in x_test], dtype=np.float32)
